7b3b390f706b8adae0e4b4ba66b5c58d3b6d528a
jouureee <jouureee@gmail.com>
2021-11-13 00:21:43 +0900

    Initial Commit

---
+++ First-RiBs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First-RiBs
+//
+//  Created by JoSoJeong on 2021/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'First-RiBs' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for First-RiBs
+pod 'RIBs', '~> 0.9'
+
+end
