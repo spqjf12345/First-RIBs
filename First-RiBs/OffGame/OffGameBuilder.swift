c918da2b3ef9f8a3c3b9ea9604b528b17bebb5f8
jouureee <jouureee@gmail.com>
2022-05-12 18:43:07 +0900

    Tutorial3 update score stream

--- First-RiBs/OffGame/OffGameBuilder.swift
+++ First-RiBs/OffGame/OffGameBuilder.swift
@@ -12,6 +12,7 @@ protocol OffGameDependency: Dependency {
     // created by this RIB.
     var player1Name: String { get }
     var player2Name: String { get }
+    var scoreStream: ScoreStream { get }
 }
 
 final class OffGameComponent: Component<OffGameDependency> {
@@ -21,9 +22,13 @@ final class OffGameComponent: Component<OffGameDependency> {
            return dependency.player1Name
        }
 
-       fileprivate var player2Name: String {
-           return dependency.player2Name
-       }
+    fileprivate var player2Name: String {
+       return dependency.player2Name
+    }
+    
+    fileprivate var scoreStream: ScoreStream {
+        return dependency.scoreStream
+    }
 }
 
 // MARK: - Builder
@@ -41,7 +46,8 @@ final class OffGameBuilder: Builder<OffGameDependency>, OffGameBuildable {
     func build(withListener listener: OffGameListener) -> OffGameRouting {
         let component = OffGameComponent(dependency: dependency)
         let viewController = OffGameViewController(player1Name: component.player1Name, player2Name: component.player2Name)
-        let interactor = OffGameInteractor(presenter: viewController)
+        let interactor = OffGameInteractor(presenter: viewController,
+                                              scoreStream: component.scoreStream)
         interactor.listener = listener
         return OffGameRouter(interactor: interactor, viewController: viewController)
     }
--- First-RiBs/OffGame/OffGameInteractor.swift
+++ First-RiBs/OffGame/OffGameInteractor.swift
@@ -13,8 +13,9 @@ protocol OffGameRouting: ViewableRouting {
 }
 
 protocol OffGamePresentable: Presentable {
-    var listener: OffGamePresentableListener? { get set }
     // TODO: Declare methods the interactor can invoke the presenter to present data.
+    var listener: OffGamePresentableListener? { get set }
+    func set(score: Score)
 }
 
 protocol OffGameListener: AnyObject {
@@ -25,21 +26,36 @@ final class OffGameInteractor: PresentableInteractor<OffGamePresentable>, OffGam
 
     weak var router: OffGameRouting?
     weak var listener: OffGameListener?
+    private let scoreStream: ScoreStream
 
     // TODO: Add additional dependencies to constructor. Do not perform any logic
     // in constructor.
-    override init(presenter: OffGamePresentable) {
+    init(presenter: OffGamePresentable,
+         scoreStream: ScoreStream) {
+        self.scoreStream = scoreStream
         super.init(presenter: presenter)
         presenter.listener = self
     }
 
+
     override func didBecomeActive() {
         super.didBecomeActive()
         // TODO: Implement business logic here.
+        updateScore()
     }
 
     override func willResignActive() {
         super.willResignActive()
         // TODO: Pause any business logic.
+    }
+    
+    private func updateScore() {
+        scoreStream.score
+            .subscribe(
+                onNext: { (score: Score) in
+                    self.presenter.set(score: score)
+                }
+            )
+            .disposeOnDeactivate(interactor: self)
     }
 }
